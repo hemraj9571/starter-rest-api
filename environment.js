758da131759e76b2ad94a002bfec898e9ce66996
hemra9571 <hemu9571782430@gmail.com>
2024-04-14 17:11:27 +0530

    api

--- environment.js
+++ environment.js
@@ -1,8 +1,8 @@
 const dev = {
     env: process.env.NODE_ENV,
 //mongoDbConnectionString: "mongodb://127.0.0.1:27017/realludoking",
-mongoDbConnectionString: "mongodb+srv://orbot:orbot@1212@cluster0.grnzrng.mongodb.net/realludoclub",
-//mongoDbConnectionString: "mongodb+srv://orbot:orbot@1212@cluster0.grnzrng.mongodb.net/ludokingjaipur",
+//mongoDbConnectionString: "mongodb+srv://orbot:orbot@1212@cluster0.grnzrng.mongodb.net/realludoclub",
+mongoDbConnectionString: "mongodb+srv://orbot:orbot@1212@cluster0.grnzrng.mongodb.net/ludokingjaipur",
 }
 
 
